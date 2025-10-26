258cfed38db62b2e2d30b3dd8ad833586cd3062c
Reazul Hoque <engrreaz@gmail.com>
2025-10-27 01:22:21 +0600

    upload

--- mailer/send-mail.php
+++ mailer/send-mail.php
@@ -17,6 +17,8 @@ try {
     $mail->Username = MAIL_FROM;   // উদাহরণ: noreply@eimbox.com
     $mail->Password = MAIL_SECRET; // ইমেইল পাসওয়ার্ড
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // SSL
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = 'html';
     $mail->Port = 465;
 
     // SSL সার্টিফিকেট যদি self-signed হয় (শুধু টেস্টের জন্য)
@@ -49,36 +51,41 @@ try {
 
     // === Replace Variables by Type ===
     if ($mail_type === 'new-ins') {
-        $html = str_replace(['%NAME%', '%ACTIVATION_LINK%', '%INSTITUTE_NAME%'], 
-                            [htmlspecialchars($mail_name), htmlspecialchars($resetLink), htmlspecialchars($mail_name)], 
-                            $htmlTemplate);
-    } 
-    else if ($mail_type === 'otp') {
-        $html = str_replace(['%NAME%', '%ACTIVATION_LINK%', '%INSTITUTE_NAME%'], 
-                            [htmlspecialchars($mail_name), htmlspecialchars($resetLink), htmlspecialchars($mail_name)], 
-                            $htmlTemplate);
+        $html = str_replace(
+            ['%NAME%', '%ACTIVATION_LINK%', '%INSTITUTE_NAME%'],
+            [htmlspecialchars($mail_name), htmlspecialchars($resetLink), htmlspecialchars($mail_name)],
+            $htmlTemplate
+        );
+    } else if ($mail_type === 'otp') {
+        $html = str_replace(
+            ['%NAME%', '%ACTIVATION_LINK%', '%INSTITUTE_NAME%'],
+            [htmlspecialchars($mail_name), htmlspecialchars($resetLink), htmlspecialchars($mail_name)],
+            $htmlTemplate
+        );
 
         $digits = str_split($token);
         for ($i = 0; $i < 6; $i++) {
             $html = str_replace('%D' . ($i + 1) . '%', $digits[$i], $html);
         }
         $html = str_replace('%OTP%', $token, $html);
-    } 
-    else if ($mail_type == 'forgot-password') {
-        $html = str_replace(['%NAME%', '%RESET_LINK%'], 
-                            [htmlspecialchars($mail_to), htmlspecialchars($resetLink)], 
-                            $htmlTemplate);
-    } 
-    else if ($mail_type == 'new-user') {
-        $html = str_replace(['%EMAIL%', '%ACTIVATION_LINK%', '%INSTITUTE_NAME%'], 
-                            [htmlspecialchars($mail_to), htmlspecialchars($resetLink), htmlspecialchars($scname)], 
-                            $htmlTemplate);
+    } else if ($mail_type == 'forgot-password') {
+        $html = str_replace(
+            ['%NAME%', '%RESET_LINK%'],
+            [htmlspecialchars($mail_to), htmlspecialchars($resetLink)],
+            $htmlTemplate
+        );
+    } else if ($mail_type == 'new-user') {
+        $html = str_replace(
+            ['%EMAIL%', '%ACTIVATION_LINK%', '%INSTITUTE_NAME%'],
+            [htmlspecialchars($mail_to), htmlspecialchars($resetLink), htmlspecialchars($scname)],
+            $htmlTemplate
+        );
     }
 
     // === Final Mail ===
     $mail->isHTML(true);
     $mail->Subject = $mail_subject;
-    $mail->Body    = $html;
+    $mail->Body = $html;
     $mail->AltBody = strip_tags($html);
 
     $mail->send();
