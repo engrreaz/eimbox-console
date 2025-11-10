307d5cc70d77f356325e6aacd6c22c14f0afd68e
Reazul Hoque <engrreaz@gmail.com>
2025-11-11 01:41:32 +0600

    upload

--- admission-login.php
+++ admission-login.php
@@ -51,8 +51,21 @@ include_once 'actions/get-sc-data.php';
 <div class="container">
 
     <div class="row">
-        <div class="col-12  text-center">
-            <?php include_once('actions/sc-header.php'); ?>
+        <div class="col-12  ">
+            <table class="mt-4 mb-6 " style="margin:auto;">
+                <tr>
+                    <td><img src="<?php echo BASE_PATH . 'logo/' . $sccode . '.png'; ?> "
+                            style="max-width:50px; max-height:50px;" />
+                    </td>
+                    <td style="width:15px; border-right:5px solid gray;"></td>
+                    <td style="width:15px;"></td>
+                    <td>
+                        <h3 class="m-0 p-0 fw-bold"><?= $scname; ?></h3>
+                        <h6 class="m-0 p-0"><?= $address; ?></h6>
+
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
 
