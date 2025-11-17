97b7099013485a1460488dcbbd87b659e2a0a336
Reazul Hoque <engrreaz@gmail.com>
2025-11-18 01:15:21 +0600

    upload

--- admission-login.php
+++ admission-login.php
@@ -7,7 +7,7 @@ require_once 'core/core-val.php';
 
 ?>
 
-
+dgdgdgdg
 
 <?php
 
@@ -120,7 +120,7 @@ require_once 'header-plain.php';
 
 
 <div class="container">
-ািুাবাবা
+
     <?php if ($sccode != '') { ?>
         <div class="row">
             <div class="col-12  ">
