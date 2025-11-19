19d438f1081b0199dbde8d8e9918f95b04ff4904
Reazul Hoque <engrreaz@gmail.com>
2025-11-20 01:43:38 +0600

    upload

--- core/upgrade-plan.php
+++ core/upgrade-plan.php
@@ -1,25 +1,24 @@
 <div class="container-xxl flex-grow-1 container-p-y">
 
-<?php 
-echo 'ABC';
-echo '<pre>';
-print_r($raw_json);
-echo '</pre>';
-echo 'XYZ';
-// JSON থেকে PHP অ্যারেতে কনভার্ট
-$array = json_decode($row_json, true);
+    <?php
+    echo 'ABC';
+    echo '<pre>';
+    print_r($raw_json);
+    echo '</pre>';
+    echo 'XYZ';
+    // JSON থেকে PHP অ্যারেতে কনভার্ট
+    $array = json_decode($row_json, true);
 
-// ফলাফল চেক
-echo '<pre>';
-print_r($array);
-echo '</pre>';
+    if ($array === null) {
+        echo 'JSON Error: ' . json_last_error_msg();
+    } else {
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+    }
 
-if ($array === null) {
-    echo 'JSON Error: ' . json_last_error_msg();
-}
-
-echo $sccode_current_package;
-?>
+    echo $sccode_current_package;
+    ?>
     Upgrade your plan .........
 
 </div>
