d34c6309ef1f7a84de374d3a88a16760bd9072d3
Reazul Hoque <engrreaz@gmail.com>
2025-11-22 00:22:28 +0600

    upload

--- ajax/upload_logo.php
+++ ajax/upload_logo.php
@@ -1,8 +1,16 @@
 <?php
 // require_once '../core/config.php';
-// require_once '../core/db.php';
+// require_once dirname(__DIR__) .  '/core/core-val.php';
+
+// echo dirname(__DIR__) .  '/core/core-val.php'; 
+
 
 $sccode = $_POST['sccode'] ?? '';
+
+$src =  '../../sign/000000.png';
+$dest =  '../../sign/' . $sccode . '.png';
+
+
 if (!$sccode) {
     echo json_encode(['status' => 'error', 'msg' => 'Invalid sccode']);
     exit;
@@ -27,7 +35,12 @@ if (!is_dir($uploadDir))
 $ext = pathinfo($_FILES['logo']['name'], PATHINFO_EXTENSION);
 $targetFile = $uploadDir . $sccode . ".png"; // সবকে png নামে রাখবো
 
+copy($src, $dest);
+
 if (move_uploaded_file($_FILES['logo']['tmp_name'], $targetFile)) {
+
+
+
     echo json_encode(['status' => 'success']);
 } else {
     echo json_encode(['status' => 'error', 'msg' => 'Failed to move uploaded file']);
