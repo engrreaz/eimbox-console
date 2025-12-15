8f2eafc0c2bdd66d971576c36788536f06be4e7d
Reazul Hoque <engrreaz@gmail.com>
2025-12-16 03:45:03 +0600

    console-fix

--- result/merge-entire-batch.php
+++ result/merge-entire-batch.php
@@ -165,7 +165,7 @@ foreach ($students as $stu) {
 }
 
 // Determine next offset
-$nextOffset = (count($students) + $offset < $total) ? ($offset + $batchSize) : null;
+$nextOffset = (count($students)  < $total) ? ($offset + $batchSize) : null;
 $data .= count($students) . '//' . $total . '//' . $nextOffset . '................';
 echo json_encode([
     'done' => true,
