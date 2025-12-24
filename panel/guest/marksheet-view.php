c192195d4c22be13a519663eb83915c7b31a7d81
Reazul Hoque <engrreaz@gmail.com>
2025-12-25 02:22:54 +0600

    console-fix

--- panel/guest/marksheet-view.php
+++ panel/guest/marksheet-view.php
@@ -57,7 +57,7 @@
             <td style="border:0; " rowspan="2">
                 <img src="https://eimbox.com/students/<?= $stid; ?>.jpg" style="border:1px solid gray; padding:2px; width:40mm;" />
             </td>
-            <td style="width:65mm; border:0;" rowspan="2">
+            <td style="width:55mm; border:0;" rowspan="2">
                 <?php
                 $slot = $sessionInfo['slot'];
                 $sql = "
