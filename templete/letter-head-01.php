3a9f196154cb835094c22737cef6b2b232ec3899
Reazul Hoque <engrreaz@gmail.com>
2025-12-28 00:21:56 +0600

    console-fix

--- templete/letter-head-01.php
+++ templete/letter-head-01.php
@@ -30,20 +30,25 @@
     }
 </style>
 
-<div>
-    <table class="lh-table">
-        <tr>
-            <td style="padding-right:10px; width:90px;">
-                <img src="https://eimbox.com/logo/<?php echo $sccode; ?>.png" width="80">
-            </td>
-            <td>
-                <div class="a"><?php echo $scname; ?></div>
-                <div class="b"><?php echo $scaddress; ?></div>
-                <div class="c">
-                    <?php echo 'Mobile : ' . $scmobile . ' | Email : ' . $scmail; ?>
-                </div>
-                <div class="c"><?php echo 'Web : ' . $scweb; ?></div>
-            </td>
-        </tr>
-    </table>
-</div>
+
+<table style="width:100%;">
+    <tr>
+        <td>
+            <table>
+                <tr>
+                    <td style="padding-right:10px; width:90px;">
+                        <img src="https://eimbox.com/logo/<?php echo $sccode; ?>.png" width="80">
+                    </td>
+                    <td>
+                        <div class="a"><?php echo $scname; ?></div>
+                        <div class="b"><?php echo $scaddress; ?></div>
+                        <div class="c">
+                            <?php echo 'Mobile : ' . $scmobile . ' | Email : ' . $scmail; ?>
+                        </div>
+                        <div class="c"><?php echo 'Web : ' . $scweb; ?></div>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
