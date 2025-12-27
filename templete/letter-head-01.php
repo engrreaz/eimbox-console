537b8697c2d2f7ef4949686873c2160f3abfde50
Reazul Hoque <engrreaz@gmail.com>
2025-12-27 23:47:55 +0600

    console-fix

--- templete/letter-head-01.php
+++ templete/letter-head-01.php
@@ -1,58 +1,55 @@
 <style>
-    #padbox {
-        margin: auto;
+    #padbox{
+        display: inline-table;   /* সবচেয়ে গুরুত্বপূর্ণ */
+        margin: 0 auto;
+        border-collapse: collapse;
     }
 
-    #padbox table,
+    #padbox,
     #padbox tr,
-    #padbox td {
+    #padbox td{
         border: 0;
+        padding: 0;
     }
 
-    .a {
-        font-size: 20px;
-        font-weight: 700;
-        font-style: normal;
-        line-height: 24px;
+    .a{
+        font-size:20px;
+        font-weight:700;
+        line-height:24px;
     }
 
-    .b {
-        font-size: 15px;
-        font-weight: 400;
-        font-style: normal;
-        line-height: 18px;
-
+    .b{
+        font-size:15px;
+        font-weight:400;
+        line-height:18px;
     }
 
-    .c {
-        font-size: 12px;
-        font-weight: 400;
-        font-style: italic;
-        line-height: 16px;
+    .c{
+        font-size:12px;
+        font-style:italic;
+        line-height:16px;
     }
 
-
-    .code {
-        text-align: center;
-        font-size: 11px;
-        font-weight: 700;
+    .code{
+        text-align:center;
+        font-size:11px;
+        font-weight:700;
     }
 </style>
 
 <div style="text-align:center;">
-
-    <table id="padbox" style="border:0; margin: auto; margin-bottom:20px;">
+    <table id="padbox">
         <tr>
-            <td>
-                <img src="https://eimbox.com/logo/<?php echo $sccode; ?>.png" width="80" ‍style="padding-right:10px;" />
+            <td style="padding-right:10px;">
+                <img src="https://eimbox.com/logo/<?php echo $sccode; ?>.png" width="80">
             </td>
             <td>
                 <div class="a"><?php echo $scname; ?></div>
                 <div class="b"><?php echo $scaddress; ?></div>
                 <div class="c">
-                    <?php echo 'Mobile : ' . $scmobile . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email : ' . $scmail; ?>
+                    <?php echo 'Mobile : '.$scmobile.' &nbsp;&nbsp; Email : '.$scmail; ?>
                 </div>
-                <div class="c"><?php echo 'Web : ' . $scweb; ?></div>
+                <div class="c"><?php echo 'Web : '.$scweb; ?></div>
             </td>
         </tr>
     </table>
