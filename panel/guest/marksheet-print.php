eae46d18234b4bdc4a52d120b67056806871c6c4
Reazul Hoque <engrreaz@gmail.com>
2025-12-27 23:22:12 +0600

    console-fix

--- panel/guest/marksheet-print.php
+++ panel/guest/marksheet-print.php
@@ -1,23 +1,25 @@
+<?php session_start(); ?>
 <style>
     @page {
         size: A4 portrait;
         margin: 12mm;
-        
+
     }
+
     body {
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         padding: 15mm;
     }
 </style>
-    <script>
-        window.onload = function () {
-            window.print();
-        };
+<script>
+    window.onload = function () {
+        // window.print();
+    };
 
-        window.onafterprint = function () {
-            window.close();
-        };
-    </script>
+    window.onafterprint = function () {
+        window.close();
+    };
+</script>
 <?php
 require 'marksheet-data.php';
 include 'marksheet-view.php';
