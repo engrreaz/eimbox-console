332d323020d1c4585da49bc24b5176573727347d
Reazul Hoque <engrreaz@gmail.com>
2026-04-27 20:00:23 +0600

    Fix Errors

--- reports/student_collection.php
+++ reports/student_collection.php
@@ -111,11 +111,11 @@ while ($cls = $classQ->fetch_assoc()):
                         ?>
 
                         <tr>
-                            <td class="small"><?= $roll ?></td>
-                            <td class="small"><?= $stid ?></td>
-                            <td class="small"><?= $prno ?></td>
-                            <td class="small" class="small"><?= $body ?></td>
-                            <td class="text-end small"><?= number_format($tak, 2) ?></td>
+                            <td class="small py-0"><?= $roll ?></td>
+                            <td class="small py-0"><?= $stid ?></td>
+                            <td class="small py-0"><?= $prno ?></td>
+                            <td class="small py-0" class="small"><?= $body ?></td>
+                            <td class="text-end small py-0"><?= number_format($tak, 2) ?></td>
                         </tr>
 
                     <?php endwhile; ?>
