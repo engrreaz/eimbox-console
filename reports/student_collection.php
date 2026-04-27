21e23c514b3dcb68e1b9a7506c7d227d371c803f
Reazul Hoque <engrreaz@gmail.com>
2026-04-27 19:37:07 +0600

    Fix Errors

--- reports/student_collection.php
+++ reports/student_collection.php
@@ -18,56 +18,57 @@ $classQ = $conn->query($sql);
 
 $grand_total = 0;
 
-while($cls = $classQ->fetch_assoc()):
+while ($cls = $classQ->fetch_assoc()):
 
     $classname = $cls['classname'];
     $sectionname = $cls['sectionname'];
 
     $sub_total = 0;
-?>
+    ?>
 
-<div class="mt-3">
-    <h6 class="text-primary">
-        Class: <?= $classname ?> | Section: <?= $sectionname ?>
-    </h6>
+    <div class="mt-3">
+        <h6 class="text-primary">
+            Class: <?= $classname ?> | Section: <?= $sectionname ?>
+        </h6>
 
-    <div class="table-responsive">
-        <table class="table table-sm table-bordered">
-            <thead>
-                <tr>
-                    <th>Roll</th>
-                    <th>Student ID</th>
-                    <th>Receipt No</th>
-                    <th>Collected By</th>
-                    <th>Particular</th>
-                    <th>Amount</th>
-                </tr>
-            </thead>
-            <tbody>
+        <div class="table-responsive">
+            <table class="table table-sm table-bordered">
+                <thead>
+                    <tr>
+                        <th>Roll</th>
+                        <th>Student ID</th>
+                        <th>Receipt No</th>
+                        <th>Collected By</th>
+                        <th>Particular</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-<?php
-    // 2️⃣ প্রতিটি class/section অনুযায়ী stpr থেকে data
-    $sss = "
+                    <?php
+                    // 2️⃣ প্রতিটি class/section অনুযায়ী stpr থেকে data
+                    $entrybyPre = '';
+                    $sss = "
         SELECT rollno, stid, prno, entryby 
         FROM stpr
         WHERE sccode='$sccode' 
         AND prdate='$date'
         AND classname='$classname'
         AND sectionname='$sectionname'
-        ORDER BY rollno ASC
+        ORDER BY entryby, rollno ASC
     ";
-    // echo $sss;
-    $stQ = $conn->query($sss);
+                    // echo $sss;
+                    $stQ = $conn->query($sss);
 
-    while($st = $stQ->fetch_assoc()):
+                    while ($st = $stQ->fetch_assoc()):
 
-        $roll = $st['rollno'];
-        $stid = $st['stid'];
-        $prno = $st['prno'];
-        $entryby = $st['entryby'];
+                        $roll = $st['rollno'];
+                        $stid = $st['stid'];
+                        $prno = $st['prno'];
+                        $entryby = $st['entryby'];
 
-        // 3️⃣ stfinance থেকে details
-        $qqq = "
+                        // 3️⃣ stfinance থেকে details
+                        $qqq = "
             SELECT particulareng, pr1 
             FROM stfinance
             WHERE stid='$stid'
@@ -75,59 +76,73 @@ while($cls = $classQ->fetch_assoc()):
             AND sccode='$sccode'
             AND pr1date='$date'
         ";
-        // echo $qqq;
-        $finQ = $conn->query($qqq);
+                        // echo $qqq;
+                        $finQ = $conn->query($qqq);
 
-        $body = ''; $tak = 0;
-        while($fin = $finQ->fetch_assoc()):
+                        $body = '';
+                        $tak = 0;
+                        while ($fin = $finQ->fetch_assoc()):
 
-            $particular = $fin['particulareng'];
-            $amount = $fin['pr1'];
+                            $particular = $fin['particulareng'];
+                            $amount = $fin['pr1'];
 
-            $sub_total += $amount;
-            $grand_total += $amount;
-            $tak += $amount;
+                            $sub_total += $amount;
+                            $grand_total += $amount;
+                            $tak += $amount;
 
 
-            $body .= $particular . ': ' . number_format($amount) . ", ";
-            
-?>
-            
+                            $body .= $particular . ': ' . number_format($amount) . ", ";
 
-<?php endwhile;?>
+                            ?>
 
-<tr>
-                    <td><?= $roll ?></td>
-                    <td><?= $stid ?></td>
-                    <td><?= $prno ?></td>
-                    <td><?= $entryby ?></td>
-                    <td class="small"><?= $body ?></td>
-                    <td class="text-end"><?= number_format($tak,2) ?></td>
-                </tr>
 
-<?php  endwhile; ?>
+                        <?php endwhile; ?>
+                        <?php
+                        if ($entrybyPre != $entryby) {
+                            $entrybyPre = $entryby;
+                            $body = "Collected by: " . $entryby . " | " . $body;
 
-            </tbody>
+                            ?>
+                            <tr>
+                                <td colspan="6"><?= $body ?></td>
+                            </tr>
+                            <?php
+                        }
 
-            <tfoot>
-                <tr>
-                    <th colspan="5" class="text-end">Sub Total</th>
-                    <th class="text-end text-success">
-                        <?= number_format($sub_total,2) ?>
-                    </th>
-                </tr>
-            </tfoot>
+                        ?>
 
-        </table>
+                        <tr>
+                            <td><?= $roll ?></td>
+                            <td><?= $stid ?></td>
+                            <td><?= $prno ?></td>
+                            <td><?= $entryby ?></td>
+                            <td class="small"><?= $body ?></td>
+                            <td class="text-end"><?= number_format($tak, 2) ?></td>
+                        </tr>
+
+                    <?php endwhile; ?>
+
+                </tbody>
+
+                <tfoot>
+                    <tr>
+                        <th colspan="5" class="text-end">Sub Total</th>
+                        <th class="text-end text-success">
+                            <?= number_format($sub_total, 2) ?>
+                        </th>
+                    </tr>
+                </tfoot>
+
+            </table>
+        </div>
     </div>
-</div>
 
 <?php endwhile; ?>
 
 <!-- GRAND TOTAL -->
 <div class="mt-4">
     <h5 class="text-end">
-        Grand Total: 
-        <span class="text-danger"><?= number_format($grand_total,2) ?></span>
+        Grand Total:
+        <span class="text-danger"><?= number_format($grand_total, 2) ?></span>
     </h5>
 </div>
