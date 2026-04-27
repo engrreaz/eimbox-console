523ad50cffef710ed18f0aaa1543e686ee3a390b
Reazul Hoque <engrreaz@gmail.com>
2026-04-28 03:04:22 +0600

    Fix Errors

--- daily-reports.php
+++ daily-reports.php
@@ -137,8 +137,19 @@ $sessionyear = $_COOKIE['chain-session'] ?? date('Y');
   $('#printBtn').click(function () {
 
     let style = `
-        <style>
+        
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+  href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Sans+Bengali:wght@100..900&family=Noto+Serif+Bengali:wght@100..900&display=swap"
+  rel="stylesheet">
+    
+    <style>
             @media print {
+             #reportArea {
+    font-family: "IBM Plex Mono", monospace;
+
+  }
                 table {
                     width: 100% !important;
                     border-collapse: collapse;
