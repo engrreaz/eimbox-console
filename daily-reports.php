b59e8ab8e02fce1929f65b93e9e7b99d88e83222
Reazul Hoque <engrreaz@gmail.com>
2026-04-27 23:45:36 +0600

    Fix Errors

--- daily-reports.php
+++ daily-reports.php
@@ -5,7 +5,15 @@ $slot = $_COOKIE['chain-slot'] ?? 'School';
 $sessionyear = $_COOKIE['chain-session'] ?? date('Y');
 ?>
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Sans+Bengali:wght@100..900&family=Noto+Serif+Bengali:wght@100..900&display=swap" rel="stylesheet">
 <style>
+
+  body {
+      font-family: "IBM Plex Mono", monospace;
+
+  }
   .action-btn {
     cursor: pointer;
     transition: .2s;
