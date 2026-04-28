80915b16b6faf9292d06851f2b998ced6400930a
Reazul Hoque <engrreaz@gmail.com>
2026-04-28 14:12:53 +0600

    Fix Errors

--- daily-reports.php
+++ daily-reports.php
@@ -146,7 +146,7 @@ $sessionyear = $_COOKIE['chain-session'] ?? date('Y');
 
           @page {
               size: A4;       
-              margin: 10mm 10mm 5mm 15mm;  /* bottom margin বাড়ানো */
+              margin: 10mm 10mm 15mm 15mm;  /* bottom margin বাড়ানো */
           }
 
           body {
