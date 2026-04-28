4b199f0ed13c4ae404b1e671f2dddec2e5cf5d7d
Reazul Hoque <engrreaz@gmail.com>
2026-04-28 17:19:54 +0600

    Fix Errors

--- exam-routine.php
+++ exam-routine.php
@@ -1,8 +1,8 @@
-<?php 
-require_once 'header.php'; 
+<?php
+require_once 'header.php';
 
 $slot = $_COOKIE['chain-slot'] ?? null;
-$sessionyear= $_COOKIE['chain-session'] ?? null;
+$sessionyear = $_COOKIE['chain-session'] ?? null;
 $classname = $_COOKIE['chain-class'] ?? null;
 $sectionname = $_COOKIE['chain-section'] ?? null;
 $exam = $_COOKIE['chain-exam'] ?? null;
@@ -14,21 +14,21 @@ $exam = $_COOKIE['chain-exam'] ?? null;
 
 <div class="container-xxl flex-grow-1 container-p-y">
   <?php
-  $chain_param = '-c 4 -t Choose Values -u -r -b View List exam';
+  $chain_param = '-c 12 -t Choose Values -u -r -b View Routine -h exam';
   include 'components/slot-tree-ui.php';
   ?>
 
 
-<div class="row">
-  <div class="card">
-    <div class="card-header">
+  <div class="row">
+    <div class="card">
+      <div class="card-header">
 
-    </div>
-    <div class="card-body">
+      </div>
+      <div class="card-body">
 
+      </div>
     </div>
   </div>
-</div>
 
 </div>
 
