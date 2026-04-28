e5c449a178c9b41dbb3c5fd712d75216dbd168be
Reazul Hoque <engrreaz@gmail.com>
2026-04-28 14:06:44 +0600

    Fix Errors

--- daily-reports.php
+++ daily-reports.php
@@ -151,7 +151,7 @@ $sessionyear = $_COOKIE['chain-session'] ?? date('Y');
 
           body {
               font-family: "IBM Plex Mono", monospace;
-              margin-bottom: 60px; /* 🔥 footer এর জন্য জায়গা */
+              margin-bottom: 45px; /* 🔥 footer এর জন্য জায়গা */
           }
 
           .print-footer {
