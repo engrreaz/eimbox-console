72ec71db29e1a195a9c957d585bd812355262830
Reazul Hoque <engrreaz@gmail.com>
2026-04-29 14:36:28 +0600

    Fix Errors

--- enroll-students.php
+++ enroll-students.php
@@ -734,7 +734,7 @@ $display_name = array("Student_Name_English", "Student_Name_Bengali", "Father_Na
         });
     }
 
-    $("#slot").on("click", function () {
+    $("#slot").on("change", function () {
 
         const slot = $(this).val();
         console.log("Slot changed:", slot);
