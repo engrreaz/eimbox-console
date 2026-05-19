136a09bc8a3c6f1f668f58ac5b7d4bd8a71e1506
Reazul Hoque <engrreaz@gmail.com>
2026-05-19 13:20:18 +0600

    Fix Errors

--- finance/get-report-by-date.php
+++ finance/get-report-by-date.php
@@ -260,17 +260,17 @@ $total_expense = 0;
 
                     <tr>
                         <td colspan="3" class="p-0 ps-4">Grand Total</td>
-                        <td class="text-end">
+                        <td class="text-end p-0">
                             <div class="alert alert-success p-0">
                                 <b><?= number_format($total_income, 2) ?></b>
                             </div>
                         </td>
-                        <td class="text-end">
+                        <td class="text-end p-0">
                             <div class="alert alert-danger p-0">
                                 <b><?= number_format($total_expense, 2) ?></b>
                             </div>
                         </td>
-                        <td class="text-end">
+                        <td class="text-end p-0">
                             <div class="alert alert-primary p-0">
                                 <b><?= number_format($total_income - $total_expense, 2) ?></b>
                             </div>
