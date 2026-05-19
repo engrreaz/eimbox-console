ef24fc3d853e8f4ec910c37cbb59ab1f47e86f90
Reazul Hoque <engrreaz@gmail.com>
2026-05-19 16:38:20 +0600

    Fix Errors

--- finance/get-report-by-date.php
+++ finance/get-report-by-date.php
@@ -246,7 +246,7 @@ $total_expense = 0;
                                 ?>
                                 <td>
                                     <?php
-                                    $descrip = $account_title_list[$row['account_sub_head']]['account_head'] ?? '--' ;
+                                    $descrip = $account_title_list[$row['account_head']]['account_head'] ?? '--' ;
                                     echo htmlspecialchars($descrip);
                                     ?>
                                 </td>
