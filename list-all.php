18e05d3e7ea1475b51cfb917695f3f7e26867550
Reazul Hoque <engrreaz@gmail.com>
2026-06-10 03:03:18 +0600

    Fix Errors

--- list-all.php
+++ list-all.php
@@ -229,7 +229,7 @@ $sl = 1; ?>
                     echo '
                     <div class="table-responsive mb-4">
 
-                        <h5 class="mb-3 text-primary text-center">
+                        <h5 class="mb-3 mt-5 text-dark text-center">
                             Session: ' . $row['sessionyear'] . ' |
                             Class: ' . $row['classname'] . ' |
                             Section: ' . $row['sectionname'] . '
