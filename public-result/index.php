5468c533452010c6bced843f81e6bc12b6b97b0a
Reazul Hoque <engrreaz@gmail.com>
2026-08-04 14:53:04 +0600

    Updating issues

---
+++ public-result/index.php
@@ -0,0 +1,64 @@
+<?php
+require_once '../header.php';
+
+if ($_SESSION['user_role'] == 'guest' && $_SESSION['userlevel'] == 'guest') {
+
+
+
+
+echo 'sfsdfsfsfs';
+
+
+
+
+
+
+
+
+
+
+
+} else {
+    require_once 'index-admin.php';
+}
+
+require_once '../footer.php'; ?>
+
+<!-- ----------------------------------- -->
+<script></script>
+<!-- ----------------------------------- -->
+
+<?php if ($_SESSION['user_role'] == 'guest' && $_SESSION['userlevel'] == 'guest') { ?>
+    <script>
+        $('.view-transcript').on('click', function () {
+            // alert('Transcript ID: <?php echo $id; ?>\nGPA: <?php echo $gpa; ?>\nGrade: <?php echo $grade; ?>\nTotal Marks: <?php echo $totalMarks; ?>');
+            let modal = new bootstrap.Modal(document.getElementById('transcriptModal'));
+            modal.show();
+        });
+    </script>
+
+
+    <script>
+        $('.print-transcript').on('click', function () {
+            window.open(
+                'panel/guest/marksheet-print.php?stid=<?= $stid ?>&id=<?= urlencode($id) ?>',
+                '_blank'
+            );
+        });
+    </script>
+
+    <script>
+        $('.download-transcript').on('click', function () {
+            window.location.href =
+                'panel/guest/marksheet-pdf.php?stid=<?= $stid ?>&id=<?= urlencode($id) ?>';
+        });
+    </script>
+
+
+
+<?php } ?>
+
+
+</body>
+
+</html>
