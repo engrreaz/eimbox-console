71d1b75804d9b0494f31c003bb2c993bac5562c7
Reazul Hoque <engrreaz@gmail.com>
2026-08-05 01:04:18 +0600

    Updating issues

--- public-result/marksheet.php
+++ public-result/marksheet.php
@@ -170,6 +170,7 @@ foreach ($subject_codes_raw as $sub_code) {
         }
     }
 
+    echo $col_prefix . ' - ' . $sub_code . ' - ' . $subject_name . ' - ' . $full_marks . ' - ' . $total_mark . '<br>'; // For debugging
     $marks_data[$sub_code] = [
         'subcode' => $sub_code,
         'subject' => $subject_name,
