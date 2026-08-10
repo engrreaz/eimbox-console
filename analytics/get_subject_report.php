2f6a9346c040fd34085a9e9a14805ab3c3371175
Reazul Hoque <engrreaz@gmail.com>
2026-08-11 02:52:54 +0600

    Updating issues

--- analytics/get_subject_report.php
+++ analytics/get_subject_report.php
@@ -5,7 +5,6 @@ require_once '../core/config.php';
 require_once '../core/db.php';
 require_once '../core/global_values.php';
 
-$sctype = $_GET['sctype'] ?? null;
 
 $dataset_id = (int)($_GET['dataset_id'] ?? 0);
 $sccode = $_SESSION['sccode'] ?? null;
