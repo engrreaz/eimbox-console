4d971873986b560ef29a8ae164b21a9224f9668b
Reazul Hoque <engrreaz@gmail.com>
2026-08-16 04:18:43 +0600

    Updating issues

--- analytics-print-all.php
+++ analytics-print-all.php
@@ -12,7 +12,7 @@ if (empty($dataset_id) || empty($sccode)) {
 }
 
 // Fetch all data using the new script
-$api_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/analytics/get_full_report_data.php?dataset_id=' . $dataset_id;
+$api_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 'analytics/get_full_report_data.php?dataset_id=' . $dataset_id;
 echo $api_url;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $api_url);
