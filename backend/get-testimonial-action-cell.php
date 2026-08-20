57402360fcb8704b3257021d0f23b83ff9c06f7f
Reazul Hoque <engrreaz@gmail.com>
2026-08-20 23:07:34 +0600

    Updating issues

--- backend/get-testimonial-action-cell.php
+++ backend/get-testimonial-action-cell.php
@@ -1,6 +1,8 @@
 <?php
 ob_start(); // Start output buffering
-require_once '../core/init.php';
+require_once '../core/config.php';
+require_once '../core/db.php';
+require_once '../core/global_values.php';
 
 $stid = $_GET['stid'] ?? null;
 $exam2 = $_GET['exam'] ?? 'SSC'; // Assuming exam is passed from frontend
--- backend/issue-testimonial.php
+++ backend/issue-testimonial.php
@@ -1,6 +1,11 @@
 <?php
 ob_start(); // Start output buffering
-require_once '../core/init.php';
+require_once '../core/config.php';
+require_once '../core/db.php';
+require_once '../core/global_values.php';
+
+
+
 header('Content-Type: application/json'); // Change header to JSON
 
 // Get data from POST request
